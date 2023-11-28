cbbc6eb64f80d453d035e8935dfb23ed3adc684a
farisASiS <faris.bakhari9895@gmail.com>
2023-11-28 21:53:41 +0800

    Lepas revert, api signup/navigate ok tpi error message x kluar

--- api/index.js
+++ api/index.js
@@ -18,7 +18,6 @@ const app = express();
 
 app.use(cors());
 
-
 app.use(express.json());
 
 app.listen(3000, () => {
--- client/src/pages/DaftarAkaun.jsx
+++ client/src/pages/DaftarAkaun.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export default function DaftarAkaun() {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData(
       {
@@ -13,20 +16,30 @@ export default function DaftarAkaun() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
+    try {
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if(data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      };
+      setLoading(false);
+      setError(null);
+      navigate('/daftar-masuk');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
-  console.log(formData);
-
-
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className = 'text-3xl text-center font-semibold my-7'>Daftar Akaun</h1>
@@ -37,7 +50,10 @@ export default function DaftarAkaun() {
         className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type="password" placeholder='kata laluan'
         className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button className='bg bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:-80'>Daftar Masuk</button>
+        <button disabled = {loading} className='bg bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 
+        disabled:-80'>
+          {loading ? 'Loading...' : 'Daftar Akaun'}
+          </button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Sudah mempunyai akaun?</p>
@@ -45,6 +61,7 @@ export default function DaftarAkaun() {
           <span className='text-blue-700'>Daftar Masuk</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
