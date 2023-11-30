0bb1defac1037f1e8066b1eab9e13f16ea471a6d
farisASiS <faris.bakhari9895@gmail.com>
2023-12-01 01:16:26 +0800

    create user update api, bcryptjs dah boleh guna

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,12 @@
-import { errorHandler } from '../../utils/error.js';
+import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password} = req.body;
-  /* const hashedPassword = bcryptjs.hashSync(password, 10); */
-  const newUser = new User({ username, email, password/* : hashedPassword */});
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword});
   try {
       await newUser.save();
       res.status(201).json('User created succesfully!')
@@ -22,8 +22,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'Pengguna tiada dalam rekod!'));
-    /* const validPassword = bcryptjs.compareSync(password, validUser.password); */ /* option ikut video,tpi problem tkleh match password */
-    const validPassword = await User.findOne({ password }); /* option kalau tak guna bcryptjs */
+    const validPassword = bcryptjs.compareSync(password, validUser.password); 
     if (!validPassword) return next(errorHandler(401, 'Kata laluan tidak sah!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
