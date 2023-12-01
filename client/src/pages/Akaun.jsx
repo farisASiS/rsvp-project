2b63b468c6d2855cb3c9557fef77229ff0f5f095
farisASiS <faris.bakhari9895@gmail.com>
2023-12-01 23:46:53 +0800

    complete update user functionality

--- client/src/pages/Akaun.jsx
+++ client/src/pages/Akaun.jsx
@@ -1,22 +1,91 @@
-import React from 'react';
+import { useState } from 'react'
 import { CgProfile } from "react-icons/cg";
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { 
+  updateUserFailure, 
+  updateUserStart, 
+  updateUserSuccess, 
+} from '../redux/user/userSlice';
 
 export default function Akaun() {
+
+  const { currentUser, loading, error } = useSelector(state => state.user);
+  const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Akaun</h1>
-      <form className='flex flex-col gap-4'>
-      <CgProfile className='h-24 w-24 self-center'/>
-        <input type="text" placeholder='nama pengguna' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-        <input type="text" placeholder='kata laluan' className='border p-3 rounded-lg' id='password' />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Kemaskini</button>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <CgProfile className='h-24 w-24 self-center'/>
+        <input 
+          type="text" 
+          placeholder='nama pengguna' 
+          defaultValue={currentUser.username}
+          className='border p-3 rounded-lg' 
+          id='username' 
+          onChange={handleChange}
+        />
+        <input 
+          type="email" 
+          placeholder='email' 
+          defaultValue={currentUser.email}
+          className='border p-3 rounded-lg' 
+          id='email' 
+          onChange={handleChange}
+        />
+        <input 
+          type="text" 
+          placeholder='kata laluan' 
+          className='border p-3 rounded-lg' 
+          id='password' 
+          onChange={handleChange}
+        />
+        <button disable={loading} className='bg-slate-700 text-white rounded-lg 
+          p-3 uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Kemaskini'}
+        </button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Hapus Akaun</span>
         <span className='text-red-700 cursor-pointer'>Daftar Keluar</span>
       </div>
+      <p className='text-red-700 mt-5' >{error ? error: ''}</p>
+      <p className='text-green-700 mt-5' >{updateSuccess ? 'Akaun berjaya dikemaskini!': ''}</p>
     </div>
   )
 }
