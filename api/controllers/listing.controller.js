7adac6dbd98109fb6bbdc7d2beb55ba22941799c
farisASiS <faris.bakhari9895@gmail.com>
2023-12-03 21:54:53 +0800

    create update acara api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -29,3 +29,24 @@ export const deleteListing = async (req, res, next) => {
     next(error)
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Acara tiada dalam rekod!'));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'Anda hanya boleh kemaskini acara anda!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
